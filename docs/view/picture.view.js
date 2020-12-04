4191e5c60e2e92d79988d1cbec848a3fb8e52ce3
Evan.G <seazeg@126.com>
2020-12-04 14:19:52 +0800

    default Commit Message [Evan.G]

--- docs/view/picture.view.js
+++ docs/view/picture.view.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-04 11:43:06
- * @LastEditTime : 2020-12-04 12:05:29
+ * @LastEditTime : 2020-12-04 12:08:51
  * @Description  :
  */
 export const viewinfo = {
@@ -13,6 +13,15 @@ hope-lg-src="http://test.haier.com/frontDEMO/ProgressiveJPEG/ProgressiveJPEG.jpg
 hope-md-src="http://test.haier.com/netcn/images/home_brand_img1.png"
 hope-sm-src="http://test.haier.com/frontDEMO/ProgressiveJPEG/ProgressiveJPEG.jpg"
 hope-xs-src="http://test.haier.com/netcn/W020201201621125480698.png" alt="">
+
+<img class="hope_picture" 
+src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607057375590&di=46c29f9e7f28f4ee253132c51d6102fb&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fc67bcdfe10d5201067157ff7376414c6a9f9900437c69-EndxN7_fw658"
+hope-xl-src="http://test.haier.com/frontDEMO/ProgressiveJPEG/990.png"
+hope-lg-src="http://test.haier.com/frontDEMO/ProgressiveJPEG/ProgressiveJPEG.jpg"
+hope-md-src="http://test.haier.com/netcn/images/home_brand_img1.png"
+hope-sm-src="http://test.haier.com/frontDEMO/ProgressiveJPEG/ProgressiveJPEG.jpg"
+hope-xs-src="http://test.haier.com/netcn/W020201201621125480698.png" alt="">
+
     `,
     js:`
 hope.picture({
--- src/module/adapimage.js
+++ src/module/adapimage.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-07 10:35:59
- * @LastEditTime : 2020-12-04 11:34:04
+ * @LastEditTime : 2020-12-04 14:15:08
  * @Description  :
  */
 
@@ -10,72 +10,81 @@ const $ = require("../utils/hopeu.js");
 module.exports.adapimageHandler = function ({ ele, options, on }) {
     const obj = new Object();
     let $dom = $(ele);
+    $dom.css({
+        opacity: 0,
+        transition: "all 0.2s",
+    });
     $dom.each(function () {
         let _this = $(this);
-        //原始
-        let e = {
-            w: _this.width(),
-            h: _this.height(),
-        };
-        let parent = {
-            w: _this.parent().width(),
-            h: _this.parent().height(),
-        };
+        getImgNaturalDimensions(_this.get(0), function (e) {
+            let parent = {
+                w: _this.parent().width(),
+                h: _this.parent().height(),
+            };
 
-        let scale = {
-            w: Math.ceil((parent.h / e.h) * e.w),
-            h: Math.ceil((parent.w / e.w) * e.h),
-        };
+            let scale = {
+                w: Math.ceil((parent.h / e.h) * e.w),
+                h: Math.ceil((parent.w / e.w) * e.h),
+            };
 
-        _this.parent().css({
-            overflow: "hidden",
-            position: "relative",
+            _this.parent().css({
+                overflow: "hidden",
+                position: "relative",
+            });
+
+            if (e.w > e.h) {
+                //以宽为准，高100%
+                if (scale.w > parent.w) {
+                    _this.css({
+                        width: "auto",
+                        height: "100%",
+                        position: "absolute",
+                        left: function () {
+                            return -((scale.w - parent.w) / 2);
+                        },
+                        top: 0,
+                    });
+                } else {
+                    _this.css({
+                        width: "100%",
+                        height: "auto",
+                        position: "absolute",
+                        top: function () {
+                            return -((scale.h - parent.h) / 2);
+                        },
+                        left: 0,
+                    });
+                }
+            } else if (e.w < e.h) {
+                //以高为准，宽100%
+                if (scale.h > parent.h) {
+                    _this.css({
+                        width: "100%",
+                        height: "auto",
+                        position: "absolute",
+                        top: function () {
+                            return -((scale.h - parent.h) / 2);
+                        },
+                        left: 0,
+                    });
+                } else {
+                    _this.css({
+                        width: "auto",
+                        height: "100%",
+                        position: "absolute",
+                        left: function () {
+                            return -((scale.w - parent.w) / 2);
+                        },
+                        top: 0,
+                    });
+                }
+            }
         });
-
-        if (e.w > e.h) {
-            //以宽为准，高100%
-            if (scale.w > parent.w) {
-                _this.css({
-                    height: "100%",
-                    position: "absolute",
-                    left: function () {
-                        return -((scale.w - parent.w) / 2);
-                    },
-                    top: 0,
-                });
-            } else {
-                _this.css({
-                    width: "100%",
-                    position: "absolute",
-                    top: function () {
-                        return -((scale.h - parent.h) / 2);
-                    },
-                    left: 0,
-                });
-            }
-        } else if (e.w < e.h) {
-            //以高为准，宽100%
-            if (scale.h > parent.h) {
-                _this.css({
-                    width: "100%",
-                    position: "absolute",
-                    top: function () {
-                        return -((scale.h - parent.h) / 2);
-                    },
-                    left: 0,
-                });
-            } else {
-                _this.css({
-                    height: "100%",
-                    position: "absolute",
-                    left: function () {
-                        return -((scale.w - parent.w) / 2);
-                    },
-                    top: 0,
-                });
-            }
-        }
     });
+
+    setTimeout(() => {
+        $dom.css("opacity", 1);
+    }, 100);
 
     if (on.init) {
         on.init({
@@ -84,24 +93,13 @@ module.exports.adapimageHandler = function ({ ele, options, on }) {
     }
 
     function getImgNaturalDimensions(oImg, callback) {
-        var nWidth, nHeight;
-        if (!oImg.naturalWidth) {
-            // 现代浏览器
-
-            nWidth = oImg.naturalWidth;
-            nHeight = oImg.naturalHeight;
+        var nImg = new Image();
+        nImg.onload = function () {
+            var nWidth = nImg.width,
+                nHeight = nImg.height;
             callback({ w: nWidth, h: nHeight });
-        } else {
-            // IE6/7/8
-            var nImg = new Image();
-
-            nImg.onload = function () {
-                var nWidth = nImg.width,
-                    nHeight = nImg.height;
-                callback({ w: nWidth, h: nHeight });
-            };
-            nImg.src = oImg.src;
-        }
+        };
+        nImg.src = oImg.src;
     }
 
     return obj;
