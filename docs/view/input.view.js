43ff127492e06d1e18a8cbd621b39dd80ca48bb9
Evan.G <seazeg@126.com>
2020-12-11 16:00:38 +0800

    [add]新增可扩展内容input功能 [Evan.G]

--- docs/view/input.view.js
+++ docs/view/input.view.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-04 11:43:06
- * @LastEditTime : 2020-08-25 10:34:33
+ * @LastEditTime : 2020-12-11 15:58:59
  * @Description  :
  */
 export const viewinfo = {
@@ -10,6 +10,10 @@ general:`
 `,
 password:`
 <input name="password" type="password" placeholder="请输入密码" value="" class="hopeui-input input_normal"
+hope-verify="required" />
+`,
+extendContent:`
+<input name="extendContent" type="text" placeholder="请输入" value="" class="hopeui-input input_normal2"
 hope-verify="required" />
 `,
 js:`
@@ -27,6 +31,16 @@ var input_normal = hope.input({
         },
     },
 });
+`,
+js2:`
+hope.input({
+    ele: '.input_normal2',
+    options:{
+        extendContent:'<button type="button" class="hopeui-btn">发送短信</button>',
+        extendContentLocation:'right'
+    }
+})
+
 `
 }
 
--- src/module/input.js
+++ src/module/input.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-07 10:35:59
- * @LastEditTime : 2020-12-10 12:05:33
+ * @LastEditTime : 2020-12-11 15:53:54
  * @Description  : 文本框
  */
 
@@ -21,77 +21,106 @@ module.exports.inputHandler = function ({ ele, options, on }) {
 
     $dom.each(function () {
         let $this = $(this);
-        if (!$this.attr("hope-type")) {
-            if (is.ie() <= 9) {
-                $this
-                    .after(
-                        `<label class="hopeui-placeholder">${
-                            $this.attr("placeholder") || "请输入"
-                        }</label>`
-                    )
-                    .parent()
-                    .css("position", "relative");
 
-                $this.next().css({
-                    lineHeight: $this.css("height") + 2,
-                    paddingLeft: $this.css("paddingLeft") + 1,
+        if (options && options.extendContent && options.extendContentLocation) {
+            if (options.extendContentLocation == "left") {
+                let newBox = $(
+                    '<div class="hopeui-relative"></div>'
+                ).insertBefore($this);
+                $this.appendTo(newBox);
+                newBox.css("width", $this.get(0).offsetWidth);
+                $this
+                    .before(options.extendContent)
+                    .prev()
+                    .addClass("hopeui-input-content-left");
+
+                let eConWidth = $(this).prev().get(0).offsetWidth;
+                $(this).css("padding-left", eConWidth + 10 + "px");
+            } else {
+                let newBox = $(
+                    '<div class="hopeui-relative"></div>'
+                ).insertAfter($this);
+                $this.appendTo(newBox);
+                newBox.css("width", $this.get(0).offsetWidth);
+                $this
+                    .after(options.extendContent)
+                    .next()
+                    .addClass("hopeui-input-content-right");
+
+                let eConWidth = $(this).next().get(0).offsetWidth;
+                $(this).css("padding-right", eConWidth + 10 + "px");
+            }
+        }
+
+        if (is.ie() <= 9) {
+            $this
+                .after(
+                    `<label class="hopeui-placeholder">${
+                        $this.attr("placeholder") || "请输入"
+                    }</label>`
+                )
+                .parent()
+                .css("position", "relative");
+
+            $this.next().css({
+                lineHeight: $this.css("height") + 2,
+                paddingLeft: $this.css("paddingLeft") + 1,
+            });
+
+            if (!$this.attr("readonly")) {
+                $this.next().click(function () {
+                    $(this).addClass("hopeui-hide").prev().focus();
                 });
 
-                if (!$this.attr("readonly")) {
-                    $this.next().click(function () {
-                        $(this).addClass("hopeui-hide").prev().focus();
-                    });
+                $this.blur(function () {
+                    let _this = $(this);
+                    if (!_this.val()) {
+                        _this.next().removeClass("hopeui-hide");
+                    }
+                });
 
-                    $this.blur(function () {
-                        let _this = $(this);
-                        if (!_this.val()) {
-                            _this.next().removeClass("hopeui-hide");
-                        }
-                    });
-
-                    $this.focus(function () {
-                        let _this = $(this);
-                        if (!_this.val()) {
-                            _this.next().addClass("hopeui-hide");
-                        }
-                    });
-                }
+                $this.focus(function () {
+                    let _this = $(this);
+                    if (!_this.val()) {
+                        _this.next().addClass("hopeui-hide");
+                    }
+                });
             }
-
-            if (is.ie() <= 11) {
-                if ($this.attr("readonly")) {
-                    $this.attr("unselectable", "on");
-                }
-            }
-
-            $this.on("blur", function () {
-                if (on && on.blur) {
-                    on.blur({
-                        targetELe: $(this).get(0),
-                        value: $(this).val(),
-                        eventName: "blur",
-                    });
-                }
-            });
-            $this.on("focus", function () {
-                if (on && on.focus) {
-                    on.focus({
-                        targetELe: $(this).get(0),
-                        value: $(this).val(),
-                        eventName: "focus",
-                    });
-                }
-            });
-            $this.on("input propertychange", function () {
-                if (on && on.input) {
-                    on.input({
-                        targetELe: $(this).get(0),
-                        value: $(this).val(),
-                        eventName: "input",
-                    });
-                }
-            });
         }
+
+        if (is.ie() <= 11) {
+            if ($this.attr("readonly")) {
+                $this.attr("unselectable", "on");
+            }
+        }
+
+        $this.on("blur", function () {
+            if (on && on.blur) {
+                on.blur({
+                    targetELe: $(this).get(0),
+                    value: $(this).val(),
+                    eventName: "blur",
+                });
+            }
+        });
+        $this.on("focus", function () {
+            if (on && on.focus) {
+                on.focus({
+                    targetELe: $(this).get(0),
+                    value: $(this).val(),
+                    eventName: "focus",
+                });
+            }
+        });
+        $this.on("input propertychange", function () {
+            if (on && on.input) {
+                on.input({
+                    targetELe: $(this).get(0),
+                    value: $(this).val(),
+                    eventName: "input",
+                });
+            }
+        });
     });
 
     obj.val = function (obj) {
