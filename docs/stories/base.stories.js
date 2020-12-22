4236789f6748a7cf1b54d7e736492f7b672e4a06
Evan.G <seazeg@126.com>
2020-12-22 14:30:31 +0800

    default Commit Message [Evan.G]

--- docs/stories/base.stories.js
+++ docs/stories/base.stories.js
@@ -1,11 +1,13 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-25 10:11:15
- * @LastEditTime : 2020-09-11 11:05:29
+ * @LastEditTime : 2020-12-22 14:21:46
  * @Description  : 
  */
 import "../docs.css";
+import "../am.css";
 import "../font/hopefont.css";
 import "../../dist/hopeui.min.css";
 import "../../dist/hopeui.min.js";
+import "../../dist/assets/jquery.js"
 import "../../dist/assets/jquery.js"
