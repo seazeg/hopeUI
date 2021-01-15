de51b99514ad9f7f5ecd37db50be860e3be7cad6
Evan.G <seazeg@126.com>
2021-01-15 14:49:35 +0800

    default Commit Message [Evan.G]

--- sync/staticapi.js
+++ sync/staticapi.js
@@ -1,10 +1,11 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-12-28 15:11:24
- * @LastEditTime : 2021-01-15 14:47:24
+ * @LastEditTime : 2021-01-15 14:48:33
  * @Description  : 
  */
 const shell = require("shelljs");
+shell.exec('gitq p')
 shell.exec('cp -r ../docs/static/mock ../hopeui')
 shell.exec('cp ../docs/static/logo.png ../hopeui')
 shell.exec('cd ../../seazeg.github.io/')
@@ -12,7 +13,4 @@ shell.exec('rm -rf ../../seazeg.github.io/hopeui')
 shell.exec('cd ../../hope_controls/')
 shell.exec('cp -rf ../hopeui ../../seazeg.github.io/')
 shell.exec('zip -q -r ../../seazeg.github.io/hopeui.zip ../dist/')
-
-// shell.exec('')
-shell.exec('cd ../../seazeg.github.io/;pwd')
-shell.exec('gitq p')
+shell.exec('cd ../../seazeg.github.io/;gitq p')
