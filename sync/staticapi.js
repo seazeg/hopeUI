b35c884a2394a4c10d158867cbc93968d78d7c30
Evan.G <seazeg@126.com>
2021-02-01 11:31:48 +0800

    default Commit Message [Evan.G]

--- sync/staticapi.js
+++ sync/staticapi.js
@@ -1,16 +1,22 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-12-28 15:11:24
- * @LastEditTime : 2021-01-15 14:50:28
+ * @LastEditTime : 2021-02-01 11:30:25
  * @Description  : 
  */
 const shell = require("shelljs");
 shell.exec('gitq p')
 shell.exec('cp -r ../docs/static/mock ../hopeui')
 shell.exec('cp ../docs/static/logo.png ../hopeui')
-shell.exec('cd ../../seazeg.github.io/')
-shell.exec('rm -rf ../../seazeg.github.io/hopeui')
+
+// shell.exec('cd ../../seazeg.github.io/')
+// shell.exec('rm -rf ../../seazeg.github.io/hopeui')
+// shell.exec('cd ../../hope_controls/')
+// shell.exec('cp -rf ../hopeui ../../seazeg.github.io/')
+// shell.exec('zip -q -r ../../seazeg.github.io/hopeui.zip ../dist/')
+// shell.exec('cd ../../seazeg.github.io/;gitq p -m "[API update]"')
+
+shell.exec('cd ../../vscode-hopeui-plugin/')
+shell.exec('rm -rf ../../vscode-hopeui-plugin/src/docs/hopeui')
 shell.exec('cd ../../hope_controls/')
-shell.exec('cp -rf ../hopeui ../../seazeg.github.io/')
-shell.exec('zip -q -r ../../seazeg.github.io/hopeui.zip ../dist/')
-shell.exec('cd ../../seazeg.github.io/;gitq p -m "[API update]"')
+shell.exec('cp -rf ../hopeui ../../vscode-hopeui-plugin/src/docs/')
