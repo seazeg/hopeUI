b132529110255c84ca360e608f070901f8c7a0b0
Evan.G <seazeg@126.com>
2021-07-01 11:46:26 +0800

    default Commit Message [Evan.G]

--- docs/view/drawer.view.js
+++ docs/view/drawer.view.js
@@ -1,13 +1,13 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-08-04 11:43:06
- * @LastEditTime : 2021-07-01 10:40:03
+ * @LastEditTime : 2021-07-01 11:45:31
  * @Description  :
  */
 export const viewinfo = {
-    html:`
-    <button type='button' onclick="opena()" class='hopeui-btn hopeui-btn-radius'>打开抽屉</button>
-    <div id="drawer">
+    html: `
+    <button type='button' class='drawe_open hopeui-btn hopeui-btn-radius'>打开抽屉</button>
+    <div class="mydrawer" style="display:none">
         <div>我是内容</div>
         <div>我是内容</div>
         <div>我是内容</div>
@@ -63,8 +63,8 @@ export const viewinfo = {
         <div>我是内容</div>
         <div>我是内容</div>
     </div>`,
-    js:`var mDrawer = hope.drawer({
-        ele: "#drawer",
+    js: `var mDrawer = hope.drawer({
+        ele: ".mydrawer",
         options: {
             placement: "right",
             width: "80%",
@@ -77,9 +77,7 @@ export const viewinfo = {
             },
         },
     });
-    
-    function opena() {
-        mDrawer.open();
-    }
-    `
-}
+
+    document.querySelectorAll('.drawe_open')[0].onclick = function(){mDrawer.open()}
+    `,
+};
