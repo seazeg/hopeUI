4867ed187f27d156983697a447f0777e8f3c0a8f
Evan.G <seazeg@126.com>
2021-09-24 16:00:08 +0800

    default Commit Message [Evan.G]

--- sync/staticapi.js
+++ sync/staticapi.js
@@ -1,12 +1,11 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-12-28 15:11:24
- * @LastEditTime : 2021-09-24 15:47:32
+ * @LastEditTime : 2021-09-24 15:59:39
  * @Description  : 
  */
 const shell = require("shelljs");
-shell.exec('git add .')
-shell.exec("git commit -m [update]");
+shell.exec('gitq p')
 shell.exec('cp -r ../docs/static/mock ../hopeui')
 shell.exec('cp -r ../docs/static/img ../hopeui')
 shell.exec('cp ../docs/static/logo.svg ../hopeui')
@@ -17,8 +16,7 @@ shell.exec('cd ../../hope_controls/')
 shell.exec('cp -rf ../hopeui ../../seazeg.github.io/')
 shell.exec('zip -q -r ../../seazeg.github.io/hopeui.zip ../dist/')
 shell.exec('cd ../../seazeg.github.io/')
-shell.exec('git add .');
-shell.exec('git commit -m [update]');
+shell.exec("gitq p");
 
 // shell.exec('cd ../../vscode-hopeui-plugin/')
 // shell.exec('rm -rf ../../vscode-hopeui-plugin/src/docs/hopeui')
