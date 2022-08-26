1adc9fcce2c5b2302af3760a64cf95f7a230a691
Evan.G <seazeg@126.com>
2022-08-26 14:51:12 +0800

    default Commit Message [Evan.G]

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : Evan.G
  * @Date         : 2020-07-29 16:20:03
- * @LastEditTime : 2020-08-26 14:07:30
+ * @LastEditTime : 2022-08-26 14:49:42
  * @Description  :
  */
 
@@ -9,9 +9,9 @@ module.exports = {
     stories: ["../docs/**/*.stories.@(js|mdx)"],
     logLevel: "debug",
     addons: [
-        '@storybook/addon-docs',
+        "@storybook/addon-docs",
         // '@storybook/addon-storysource',
-        "@storybook/addon-knobs",
+        // "@storybook/addon-controls",
         "@storybook/addon-viewport",
         // "@storybook/addon-notes/register-panel",
     ],
